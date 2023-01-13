9874f3a4308ee26535b69ad1b6e8cd95eac2544f
ilyacnt <ilyagt2012@gmail.com>
2023-01-13 10:44:13 +0300

    some code refactoring

--- code.ts
+++ code.ts
@@ -5,7 +5,6 @@ figma.clientStorage.getAsync('savedData').then(data => {
 })
 
 
-
 figma.ui.onmessage = msg => {
 
   if (msg.type === 'cancel') {
@@ -16,11 +15,9 @@ figma.ui.onmessage = msg => {
 
     const {designerColor, offer, buyer, sequenceFrom, orderBy, numberOfResizes} = msg.formData
     
-
     const renameNode = (node: SceneNode, number: String | Number) => {
       node.name = `${offer}_${buyer}_${designerColor}_${number}_${(node.width).toFixed(0)}x${(node.height).toFixed(0)}`
     }
-
 
     figma.clientStorage.setAsync('savedData', {
       designerColor,
@@ -30,102 +27,55 @@ figma.ui.onmessage = msg => {
       numberOfResizes
     })
 
-    
-    let selectedFrames = figma.currentPage.selection
-    selectedFrames = selectedFrames.filter(frame => frame.type === 'FRAME') as FrameNode[]
-
+    let selectedFrames = figma.currentPage.selection.filter(frame => frame.type === 'FRAME') as FrameNode[]
 
     if (selectedFrames.length <= 0) {
 
       figma.notify('Nothing selected')
 
-    } else if (orderBy === 'layerPanel') {
-      
-      let counter = sequenceFrom
-      let counterResizes = 1
-      selectedFrames.forEach(frame => {
-        
-        renameNode(frame, counter)
-        if (counterResizes < numberOfResizes) {
-          counterResizes++
-          counter = counter
-        }
-        else if (counterResizes >= numberOfResizes) {
-          counterResizes = 1
-          counter++
-        }
-      })
-      
-
     } else if (orderBy === 'topToBottom') {
-
-      // @ts-ignore
-      selectedFrames.sort((a, b) => {
-        if (a.x < b.x) return -1;
-        if (a.x > b.x) return 1;
-
-        if (a.y < b.y) return -1;
-        if (a.y > b.y) return 1;
-      })
-
-
-      let counter = sequenceFrom
-      let counterResizes = 1
-      selectedFrames.forEach(frame => {
-        
-        renameNode(frame, counter)
-        if (counterResizes < numberOfResizes) {
-          console.log('1 IF');
-          counterResizes++
-          counter = counter
-        }
-        else if (counterResizes >= numberOfResizes) {
-          console.log('2 IF');
-          counterResizes = 1
-          counter++
-        }
-      })
-      
-      
+      sortFramesTopToBottom(selectedFrames)
     } else if (orderBy === 'leftToRight') {
-      
-      // @ts-ignore
-      selectedFrames.sort((a, b) => {
-        if (a.y < b.y) return -1;
-        if (a.y > b.y) return 1;
-        
-        if (a.x < b.x) return -1;
-        if (a.x > b.x) return 1;
-      })
-
-      let counter = sequenceFrom
-      let counterResizes = 1
-      selectedFrames.forEach(frame => {
-        
-        renameNode(frame, counter)
-        if (counterResizes < numberOfResizes) {
-          console.log('1 IF');
-          counterResizes++
-          counter = counter
-        }
-        else if (counterResizes >= numberOfResizes) {
-          console.log('2 IF');
-          counterResizes = 1
-          counter++
-        }
-      })
-
-
+      sortFramesLeftToRight(selectedFrames)
     }
 
+    let counter = sequenceFrom
+    let counterResizes = 1
+    selectedFrames.forEach(frame => {
+      renameNode(frame, counter)
+      if (counterResizes < numberOfResizes) {
+        counterResizes++
+        counter = counter
+      }
+      else if (counterResizes >= numberOfResizes) {
+        counterResizes = 1
+        counter++
+      }
+    })
+
     figma.notify('Frames renamed successfully')  
-    
-
-
   }
 
-
-
-
-  // figma.closePlugin();
 };
+
+const sortFramesTopToBottom = (frames: FrameNode[]) => {
+  // @ts-ignore
+  return frames.sort((a, b) => {
+    if (a.x < b.x) return -1;
+    if (a.x > b.x) return 1;
+
+    if (a.y < b.y) return -1;
+    if (a.y > b.y) return 1;
+  })
+}
+
+const sortFramesLeftToRight = (frames: FrameNode[]) => {
+  // @ts-ignore
+  return frames.sort((a, b) => {
+    if (a.y < b.y) return -1;
+    if (a.y > b.y) return 1;
+    
+    if (a.x < b.x) return -1;
+    if (a.x > b.x) return 1;
+  })
+}
