52c5acff78e2e726f390aeb1b4d224a2536d0edc
ilyacnt <ilyagt2012@gmail.com>
2023-08-11 12:22:05 +0300

    Start New Version Dev

--- src/components/Default.tsx
+++ src/components/Default.tsx
@@ -1,87 +1,82 @@
-import React, { useEffect } from "react";
-import Select from "../ui/Select";
-import Input from "../ui/Input";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { fetchOffers } from "../store/reducerOffersActions";
-import {
-  setCreoType,
-  setDesignerColor,
-  setOffer,
-  setOrderBy,
-} from "../store/reducerDefaultActions";
-import { setBuyer } from "../store/reducerDefaultActions";
-import { setFrom } from "../store/reducerDefaultActions";
-import Button from "../ui/Button";
+import React, { useEffect } from 'react'
+import Select from '../ui/Select'
+import Input from '../ui/Input'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { fetchOffers } from '../store/reducerOffersActions'
+import { setCreoType, setDesignerColor, setOffer, setOrderBy } from '../store/reducerDefaultActions'
+import { setBuyer } from '../store/reducerDefaultActions'
+import { setFrom } from '../store/reducerDefaultActions'
+import Button from '../ui/Button'
 
 const Default = () => {
-  const { offer, designerColor, buyer, creoType, from, orderBy } = useSelector(
-    (state) => state.default
-  );
-  const offersData = useSelector((state) => state.offers.offers);
-  const dispatch = useDispatch();
-  const creativesTypeData = ["stat", "neu", "rekl"];
-  const orderByData = ["Top to Bottom", "Left to Right", "Layer Panel"];
+    const { offer, designerColor, buyer, creoType, from, orderBy } = useSelector((state) => state.default)
+    const offersData = useSelector((state) => state.offers.offers)
+    const dispatch = useDispatch()
+    const creativesTypeData = ['stat', 'neu', 'rekl', 'krsl', 'prod', 'fvr']
+    const orderByData = ['Top to Bottom', 'Left to Right', 'Layer Panel']
 
-  const state = useSelector((state) => state);
+    const state = useSelector((state) => state)
 
-  useEffect(() => {
-    dispatch(fetchOffers());
-    // console.log(state);
-    
-  }, []);
+    useEffect(() => {
+        dispatch(fetchOffers())
+        // console.log(state);
+    }, [])
 
-  const renameHandle = () => {
-    parent.postMessage({ pluginMessage: { type: "default" } }, "*");
-  }
+    const renameHandle = () => {
+        parent.postMessage({ pluginMessage: { type: 'default' } }, '*')
+    }
 
-  const cancelHandle = () => {
-    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
-  }
+    const cancelHandle = () => {
+        parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+    }
 
+    return (
+        <div className="main-container default-container">
+            <Select
+                type="Search"
+                label="Offer:"
+                placeholder="Search..."
+                options={offersData}
+                onSelect={setOffer}
+                value={offer}
+            />
+            <Input
+                label="Designer&nbsp;color:"
+                placeholder="Sapphire"
+                value={designerColor}
+                onInput={setDesignerColor}
+            />
+            <Input label="Buyer:" placeholder="AK" value={buyer} onInput={setBuyer} />
+            <Select
+                type="Search"
+                label="Creo&nbsp;type:"
+                placeholder="Search..."
+                options={creativesTypeData}
+                onSelect={setCreoType}
+                value={creoType}
+            />
+            <div style={{ display: 'flex' }}>
+                {/* LOGO HERE */}
+                <Select
+                    type="Search"
+                    label="Order&nbsp;by:"
+                    placeholder="Search..."
+                    options={orderByData}
+                    onSelect={setOrderBy}
+                    value={orderBy}
+                />
+            </div>
+            <div className="buttons-container">
+                <Button type="secondary" onButton={cancelHandle}>
+                    Cancel
+                </Button>
+                <Button type="primary" onButton={renameHandle}>
+                    Rename
+                </Button>
+            </div>
+        </div>
+    )
+}
 
-  return (
-    <div className="main-container default-container">
-      <Select
-        type="Search"
-        label="Offer:"
-        placeholder="Search..."
-        options={offersData}
-        onSelect={setOffer}
-        value={offer}
-      />
-      <Input
-        label="Designer&nbsp;color:"
-        placeholder="Sapphire"
-        value={designerColor}
-        onInput={setDesignerColor}
-      />
-      <Input label="Buyer:" placeholder="AK" value={buyer} onInput={setBuyer} />
-      <Select
-        type="Search"
-        label="Creo&nbsp;type:"
-        placeholder="Search..."
-        options={creativesTypeData}
-        onSelect={setCreoType}
-        value={creoType}
-      />
-      <div style={{ display: "flex" }}>
-        {/* LOGO HERE */}
-        <Select
-          type="Search"
-          label="Order&nbsp;by:"
-          placeholder="Search..."
-          options={orderByData}
-          onSelect={setOrderBy}
-          value={orderBy}
-        />
-      </div>
-      <div className="buttons-container">
-        <Button type="secondary" onButton={cancelHandle}>Cancel</Button>
-        <Button type="primary" onButton={renameHandle}>Rename</Button>
-      </div>
-    </div>
-  );
-};
-
-export default Default;
+export default Default
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -4,19 +4,19 @@ import { useSelector } from 'react-redux'
 import { setNumberOfResizes } from '../store/reducerSettingsActions'
 
 const Settings = () => {
-  const { numberOfResizes } = useSelector((state) => state.settings)
+    const { numberOfResizes } = useSelector((state) => state.settings)
 
-  return (
-    <div className="main-container settings-container">
-      <Input
-        label="Number&nbsp;of&nbsp;resizes: "
-        placeholder="3"
-        onInput={setNumberOfResizes}
-        value={numberOfResizes}
-      />
-      <span className="version">Version 2.0.5</span>
-    </div>
-  )
+    return (
+        <div className="main-container settings-container">
+            <Input
+                label="Number&nbsp;of&nbsp;resizes: "
+                placeholder="3"
+                onInput={setNumberOfResizes}
+                value={numberOfResizes}
+            />
+            <span className="version">Version 2.1.0</span>
+        </div>
+    )
 }
 
 export default Settings
