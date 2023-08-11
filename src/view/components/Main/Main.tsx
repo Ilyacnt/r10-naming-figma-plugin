ba6bc1acae96b764bbfd5d051ac4fc0910dce2e1
ilyacnt <ilyagt2012@gmail.com>
2023-08-11 18:46:54 +0300

    AutoComplete Base

--- src/view/components/Main/Main.tsx
+++ src/view/components/Main/Main.tsx
@@ -1,12 +1,21 @@
-import { Space, Typography, Input } from 'antd'
+import { Space, Typography, Input, AutoComplete } from 'antd'
 import React from 'react'
+
+const options = [{ value: 'HOFF_' }, { value: 'GOK_' }, { value: 'YANDEXMAPS_' }]
 
 const Main = () => {
     return (
         <div>
-            <Space direction="horizontal">
+            <Space direction="horizontal" style={{ width: '100%' }}>
                 <Typography.Text>Offer</Typography.Text>
-                <Input placeholder="Choose offer..." />
+                <AutoComplete
+                    style={{ width: '100%' }}
+                    options={options}
+                    placeholder="try to type `b`"
+                    filterOption={(inputValue, option) =>
+                        option!.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                    }
+                />
             </Space>
         </div>
     )
